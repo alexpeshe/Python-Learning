25e8964ce8725068d6f42feb9ab71e29d8932e12
Aleksandar Peshev <87254913+alexpeshe@users.noreply.github.com>
2024-11-17 12:59:16 +0000

    Add files via upload

---
+++ 100 Days of Python projects/Day 1 projects/day1_final_project.py
@@ -0,0 +1,9 @@
+#line 2 contains a greetting for the user
+print("Hello User")
+
+#Line 5 contains of the variable a which equals the input plus the \n input cursor showing the answers for a and b on a new line
+a = input("What is the name of the city you grew up in?\n")
+b = input("What was the name of your first pet?\n")
+
+#line 9 contains of the space and the suggested answer
+print("Your band name could be " + a + " " + b)
